bc98f354e3d7fd456748ce863909d76ad56264cc
Yeen181 <vuyen181@gmail.com>
2024-01-05 00:33:59 +0700

    oop

---
+++ OOP_01_soPhuc/src/oop_01/Program.java
@@ -0,0 +1,10 @@
+package oop_01;
+
+public class Program {
+    public static void main(String[] args) {
+        SoPhuc soPhuc = new SoPhuc();
+        soPhuc.InThongTin();
+        SoPhuc soPhuc1 = new SoPhuc(1,0.008);
+        soPhuc1.InThongTin();
+    }
+}
---
+++ OOP_01_soPhuc/src/oop_01/SoPhuc.java
@@ -0,0 +1,40 @@
+package oop_01;
+
+import java.util.Scanner;
+
+public class SoPhuc {
+    private double phanThuc;
+    private double phanAo;
+
+    public double getPhanThuc() {
+        return phanThuc;
+    }
+
+    public void setPhanThuc(double phanThuc) {
+        this.phanThuc = phanThuc;
+    }
+
+    public double getPhanAo() {
+        return phanAo;
+    }
+
+    public void setPhanAo(double phanAo) {
+        this.phanAo = phanAo;
+    }
+
+    public SoPhuc(double phanThuc, double phanAo) {
+        this.phanThuc = phanThuc;
+        this.phanAo = phanAo;
+    }
+    private Scanner sc = new Scanner(System.in);
+    public SoPhuc() {
+        System.out.println("nhap phan thuc: ");
+        phanThuc = sc.nextDouble();
+        System.out.println("nhap phan ao: ");
+        phanAo = sc.nextDouble();
+    }
+
+    public void InThongTin(){
+        System.out.println(phanThuc + phanAo);
+    }
+}
