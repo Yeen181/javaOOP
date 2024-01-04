c01b052157a14c08680a5cc0ec89e927feb2f7e0
Yeen181 <vuyen181@gmail.com>
2024-01-05 01:11:54 +0700

    oop

---
+++ OOP_05_nhanVien/src/oop_05/NhanVien.java
@@ -0,0 +1,68 @@
+package oop_05;
+
+import java.util.Scanner;
+
+public class NhanVien {
+    private int maNhanVien;
+    private String hoVaTen;
+    private String phongBan;
+    private String diaChi;
+    private String soDienThoai;
+
+    public int getMaNhanVien() {
+        return maNhanVien;
+    }
+
+    public void setMaNhanVien(int maNhanVien) {
+        this.maNhanVien = maNhanVien;
+    }
+
+    public String getHoVaTen() {
+        return hoVaTen;
+    }
+
+    public void setHoVaTen(String hoVaTen) {
+        this.hoVaTen = hoVaTen;
+    }
+
+    public String getPhongBan() {
+        return phongBan;
+    }
+
+    public void setPhongBan(String phongBan) {
+        this.phongBan = phongBan;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+    private Scanner sc = new Scanner(System.in);
+    public NhanVien(){
+        System.out.println("nhap ma nhan vien");
+        maNhanVien = sc.nextInt();
+        sc.nextLine();
+        System.out.println("nhap ho va ten");
+        hoVaTen = sc.nextLine();
+        System.out.println("nhap phong ban");
+        phongBan = sc.nextLine();
+        System.out.println("nhap dia chi");
+        diaChi = sc.nextLine();
+        System.out.println("nhap so dien thoai");
+        soDienThoai = sc.nextLine();
+    }
+    public void InThongTin(){
+        System.out.println("Nhan vien " + hoVaTen + " thuoc phong ban " + phongBan + " co so dien thoai la " + soDienThoai);
+    }
+}
---
+++ OOP_05_nhanVien/src/oop_05/Program.java
@@ -0,0 +1,8 @@
+package oop_05;
+
+public class Program {
+    public static void main(String[] args) {
+        NhanVien nv = new NhanVien();
+        nv.InThongTin();
+    }
+}
