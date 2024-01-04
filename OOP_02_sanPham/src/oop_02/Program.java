7fb66574e6f77e2b2c1bc9c33df5a3345457d910
Yeen181 <vuyen181@gmail.com>
2024-01-05 01:03:38 +0700

    oop

---
+++ OOP_02_sanPham/src/oop_02/Program.java
@@ -0,0 +1,8 @@
+package oop_02;
+
+public class Program {
+    public static void main(String[] args) {
+        SanPham sp = new SanPham(1,"mi tom","an lien",true);
+        sp.InThongTin();
+    }
+}
---
+++ OOP_02_sanPham/src/oop_02/SanPham.java
@@ -0,0 +1,53 @@
+package oop_02;
+
+import java.util.Scanner;
+
+public class SanPham {
+    private int maSanPham;
+    private String tenSanPham;
+    private String loaiSanPham;
+    private boolean laSanPhamMoi;
+
+    public int getMaSanPham() {
+        return maSanPham;
+    }
+
+    public void setMaSanPham(int maSanPham) {
+        this.maSanPham = maSanPham;
+    }
+
+    public String getTenSanPham() {
+        return tenSanPham;
+    }
+
+    public void setTenSanPham(String tenSanPham) {
+        this.tenSanPham = tenSanPham;
+        laSanPhamMoi = isLaSanPhamMoi();
+    }
+
+    public String getLoaiSanPham() {
+        return loaiSanPham;
+    }
+
+    public void setLoaiSanPham(String loaiSanPham) {
+        this.loaiSanPham = loaiSanPham;
+    }
+
+    public boolean isLaSanPhamMoi() {
+        return laSanPhamMoi;
+    }
+
+    public void setLaSanPhamMoi(boolean laSanPhamMoi) {
+        this.laSanPhamMoi = laSanPhamMoi;
+    }
+
+    public SanPham(int maSanPham, String tenSanPham, String loaiSanPham, boolean laSanPhamMoi) {
+        this.maSanPham = maSanPham;
+        this.tenSanPham = tenSanPham;
+        this.loaiSanPham = loaiSanPham;
+        this.laSanPhamMoi = laSanPhamMoi;
+    }
+    public void InThongTin(){
+        System.out.println("San pham "+ tenSanPham+" co ma san pham "+ maSanPham+", thuoc loai "+ loaiSanPham +","+ laSanPhamMoi + " la san pham moi");
+    }
+}
