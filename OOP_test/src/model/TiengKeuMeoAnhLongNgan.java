b3976f78c2e9c974cacccbff83ddcd4482ea01b8
Yeen <126402513+Yeen181@users.noreply.github.com>
2024-01-16 22:32:55 +0700

    Delete OOP_test directory

--- OOP_test/src/model/TiengKeuMeoAnhLongNgan.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class TiengKeuMeoAnhLongNgan implements TiengKeu {
-    public void keu(){
-        System.out.println("Mew mew");
-    }
-}
--- OOP_test/src/model/TiengKeuMeoLongDai.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class TiengKeuMeoLongDai implements TiengKeu{
-    public void keu() {
-        System.out.println("Mèo méo meo mèo meo");
-    }
-}
